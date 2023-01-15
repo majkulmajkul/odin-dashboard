597bfaee4d6905beeba8a71a86d4280d03b8ca6e
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-15 19:34:06 +0100

    Header done

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,40 @@
+import "./Header.css";
+import { IoSearch, IoNotificationsOutline } from "react-icons/io5";
+import { Button, ProfilePic } from "./Utils";
+
 export default function Header() {
     return (
         <div className="main-header">
-
+            <div className="header-line-one">
+                <div className="search-icon">
+                    <IoSearch />
+                </div>
+                <div className="search-bar">
+                    <input type="text" />
+                </div>
+                <div className="notifications-icon">
+                    <IoNotificationsOutline />
+                </div>
+                <ProfilePic
+                    className="profile-pic-small"
+                    size="s"
+                    url="https://thumbs.dreamstime.com/b/illustration-man-cartoon-head-profile-pattern-pop-art-comic-style-side-view-pop-art-92019162.jpg"
+                />
+                <h3 className="full-name">Morgan Oakley</h3>
+            </div>
+            <div className="header-line-two">
+                <ProfilePic
+                    size="m"
+                    url="https://thumbs.dreamstime.com/b/illustration-man-cartoon-head-profile-pattern-pop-art-comic-style-side-view-pop-art-92019162.jpg"
+                />
+                <div>
+                    <p>Hi there,</p>
+                    <h3>Morgan Oakley (@morgan)</h3>
+                </div>
+                <Button name="New" />
+                <Button name="Upload" />
+                <Button name="Share" />
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/Utils.jsx
@@ -0,0 +1,18 @@
+import "./Utils.css"
+
+function Button(props) {
+    return (
+        <button className="custom-button">{props.name}</button>
+    )
+}
+
+function ProfilePic(props) {
+    return (
+        <div className={props.className}>
+            <img className={`profile-pic-${props.size}`} src={props.url} alt="Profile pic" />
+        </div>
+
+    )
+}
+
+export { Button, ProfilePic }
