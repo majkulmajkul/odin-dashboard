f5ffd05bb0d1a7b2a6a726e681d7fa0654edaf53
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-16 21:12:19 +0100

    Work on 'Projects'

--- src/Project.jsx
+++ src/Project.jsx
@@ -10,10 +10,10 @@ export default function Project(props) {
         <div className="project-card">
             <h5>{projectTitle}</h5>
             <p>{projectText}</p>
-            <div>
-                <AiFillEye />
-                <AiOutlineStar />
-                <AiOutlineFork />
+            <div className="project-card-icons">
+                <AiFillEye size="1.2rem" />
+                <AiOutlineStar size="1.2rem" />
+                <AiOutlineFork size="1.2rem" />
             </div>
         </div>
 
